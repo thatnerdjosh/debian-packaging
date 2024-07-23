9290c3ae3d9feeedd6ca954a0d908b9cd8a67264
Josh Santos <josh@santos.cloud>
2024-07-23 14:59:17 +0700

    Initial Commit

---
+++ sbuild/setup_sbuild.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# START Interface/Tooling
+CACHE_DIR=~/.cache/sbuild
+DEB_VARIANT=bullseye
+MIRROR=http://mirror.infomaniak.com/debian
+TOOL=debootstrap
+
+make_container() {
+	if [ "$TOOL" != "debootstrap" ]; then
+		print_message "$TOOL is currently not supported, exiting.";
+		exit 1;
+	fi
+
+	mkdir -p $CACHE_DIR;
+	# TODO: Add mmdebstrap for other distro support (https://wiki.debian.org/sbuild) mmdebstrap --variant=buildd unstable ~/.cache/sbuild/unstable-amd64.tar.zst;
+
+	sbuild-createchroot --include=eatmydata,ccache $DEB_VARIANT $CACHE_DIR/$DEB_VARIANT $MIRROR
+}
+
+print_message() {
+	(set +x; echo "$1") 2>/dev/null;
+}
+# END Interface/Tooling
+
+# START Script
+# Install required packages
+sudo apt install $TOOL sbuild uidmap
+
+if ! command -v $TOOL >/dev/null 2>&1; then
+  print_message "$TOOL does not exist, exiting.";
+  exit 1;
+fi
+
+# Check for existing cache directory
+if [ ! -d $CACHE_DIR ]; then
+  print_message "$CACHE_DIR does not exist, creating."
+  make_container  # TODO: Ensure error handling for makeContainer
+fi
+
+# Check for existing sbuildrc file
+if [ ! -f ~/.sbuildrc ]; then
+  print_message "Outputting sbuildrc.conf to $HOME/.sbuildrc"
+  cat sbuildrc.conf > ~/.sbuildrc
+fi
+
+print_message "You're ready to sbuild! :)"
+exit 0;
+# END Script
